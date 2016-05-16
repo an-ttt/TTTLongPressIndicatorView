1e6067a8e9a518a5a1b3a03c6a773cdb9867df07
an_ttt <406084225@qq.com>
2016-05-16 23:26:24 +0800

    Initial Commit

---
+++ TTTLongPressIndicatorView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TTTLongPressIndicatorView
+//
+//  Created by an_ttt on 16/5/16.
+//  Copyright © 2016年 an_ttt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
